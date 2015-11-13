839b6492005bd36637faa1df947e7767a2b0e02c
Sofia Cole <sofia.cole@net-a-porter.com>
2015-11-13 21:55:07 +0000

    Fixing adding updates to the actor

--- app/model/Updates.scala
+++ app/model/Updates.scala
@@ -3,10 +3,11 @@ package model
 import akka.actor.ActorRef
 import domain._
 import play.api.Play.current
-import play.api.libs.json.JsString
+import play.api.libs.json._
 import play.api.libs.ws._
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.util.Success
 
 object Updates {
@@ -15,41 +16,25 @@ object Updates {
   var currentTime:Long = 0
 
   def instagram(mediaUpdate: Seq[UpdateResponse], clientID: String, actors: List[ActorRef]): Unit = {
-
-    val noOfPosts = mediaUpdate.length
-
-    def getPost(mediaUpdate: Seq[UpdateResponse], clientID: String, index: Int, actors: List[ActorRef]): Unit = {
-
-      if(index > 0) {
-        getMedia(mediaUpdate.head.objectId, clientID, index, actors)
-        getPost(mediaUpdate, clientID, index - 1, actors)
-      }
-    }
-    getPost(mediaUpdate, clientID, noOfPosts, actors)
+    getMedia(mediaUpdate.head.objectId, clientID, actors)
   }
 
-  private def getMedia(objectID: String, clientID: String, index: Int, actors: List[ActorRef]): Unit = {
-      val url = s"https://api.instagram.com/v1/tags/netaporter/media/recent?client_id=$clientID"
+  private def getMedia(objectID: String, clientID: String, actors: List[ActorRef]): Unit = {
+      val url = s"https://api.instagram.com/v1/tags/$objectID/media/recent?client_id=$clientID"
 
-      val response = for {
+      val response: Future[Option[JsValue]] = for {
         response <- WS.url(url).get()
       } yield {
-          (response.json \\ "data").flatMap(_ \\ "images").flatMap(_ \\ "standard_resolution").flatMap(_ \\ "url")
+          (response.json \\ "data").flatMap(_ \\ "images").flatMap(_ \\ "standard_resolution").flatMap(_ \\ "url").headOption
         }
 
       response.onComplete {
-        case Success(images) =>
-          images.take(index) foreach {
-            case JsString(url) =>
-              actors foreach { actor =>
-                actor ! url
-              }
-            case _ => println("NO")
+        case Success(Some(JsString(image))) => {
+          actors foreach { actor =>
+            actor ! url
           }
-
-        case _ => println("NO SUBSCRIPTION MADE")
+        }
+        case _ => println("NO")
       }
-
   }
-
 }
